14b9c22f011924b947b0ee34b1469e0355049d42
Eduarda Araujo <duda2019h@gmail,com>
2023-12-06 21:49:17 -0300

    footer alterado

--- site/footer.php
+++ site/footer.php
@@ -1,13 +1,5 @@
 <!DOCTYPE html>
 <html lang="en">
-<style>
-    .collorFooter {
-        background-color: #BDBFC1;
-    }
-    .collorLetra{
-      color: black;
-    }
-</style>
 
 <head>
     <meta charset="UTF-8">
@@ -18,26 +10,40 @@
         integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
     <link rel="stylesheet" href="/css/navbar.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
+    <style>
+        body {
+            display: flex;
+            flex-direction: column;
+            min-height: 100vh;
+            margin: 0;
+        }
 
+        .collorFooter {
+            background-color: #BDBFC1;
+            margin-top: auto;
+        }
+
+        .collorLetra {
+            color: black;
+        }
+    </style>
 </head>
 
 <body>
 
-    <footer class="collorFooter text-white fixed-bottom text-center py-3">
+    <footer class="collorFooter text-white text-center py-3">
 
         <a href="https://www2.ufjf.br/minhaufjf/bolsas-e-oportunidades/" target="_blank">
-            <img src="../img/logoUfjf.png" alt="logoUfjf" width="55px" height="50px"
-                class="d-inline-block align-text-top">
+            <img src="../img/logoUfjf.png" alt="logoUfjf" width="55px" height="50px" class="d-inline-block align-text-top">
         </a>
         <br>
         <div class="collorLetra">
-        Home &bull; Contato <br>
-        &copy; 2023 Copyright: BolsaFacil. Todos os direitos reservados.
-         </div>
+            Home &bull; Contato <br>
+            &copy; 2023 Copyright: BolsaFacil. Todos os direitos reservados.
+        </div>
     </footer>
 
-    <script type="module"
-        src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+    <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
         integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r"
         crossorigin="anonymous"></script>
