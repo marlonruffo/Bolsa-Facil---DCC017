8cee932be214d7734d3ebfd99ee6b1db74f6c904
Maria Clara Ribeiro <mcrm74161@gmail.com>
2023-12-06 21:38:34 -0300

    teste

--- site/gerenciamento.php
+++ site/gerenciamento.php
@@ -236,41 +236,6 @@
                         </button>
                     </div>
                     <div class="modal-body">
-                        <div class="form-row">
-                            <div class="form-group col-md-6">
-                                <label for="inputField1">Título:</label>
-                                <input type="text" class="form-control" id="inputField1" placeholder="Digite algo...">
-                            </div>
-                            <div class="form-group col-md-6">
-                                <label for="inputField1">Professor responsável:</label>
-                                <input type="text" class="form-control" id="inputField1" placeholder="Digite algo...">
-                            </div>
-                            <div class="form-group col-md-6">
-                                <label for="inputField2">Vagas remuneradas:</label>
-                                <input type="email" class="form-control" id="inputField2" placeholder="Digite algo...">
-                            </div>
-                        </div>
-                        <div class="form-row">
-                            <div class="form-group col-md-6">
-                                <label for="inputField3">Vagas voluntárias:</label>
-                                <input type="number" class="form-control" id="inputField3" placeholder="Digite algo...">
-                            </div>
-                            <div class="form-group col-md-6">
-                                <label for="inputField4">Data Ingresso:</label>
-                                <input type="date" class="form-control" id="inputField4" placeholder="Digite algo...">
-                            </div>
-                        </div>
-                        <div class="form-row">
-                            <div class="form-group col-md-6">
-                                <label for="inputField5">Telefone:</label>
-                                <input type="text" class="form-control" id="inputField5" placeholder="Digite algo...">
-                            </div>
-                            <div class="form-group col-md-6">
-                                <label for="inputField6">Matrícula:</label>
-                                <input type="text" class="form-control" id="inputField6" placeholder="Digite algo...">
-                            </div>
-                        </div>
-                    </div>
                     <div class="modal-footer">
                         <button type="submit" class="btn btn-primary botaoSalvarModal">Editar</button>
                         <button type="button" class="btn btn-secondary" data-dismiss="modal">Fechar</button>
---
+++ site/modal_edita_projeto.php
@@ -0,0 +1,35 @@
+<div class="form-row">
+                            <div class="form-group col-md-6">
+                                <label for="inputField1">Título:</label>
+                                <input type="text" class="form-control" id="inputField1" placeholder="Digite algo...">
+                            </div>
+                            <div class="form-group col-md-6">
+                                <label for="inputField1">Professor responsável:</label>
+                                <input type="text" class="form-control" id="inputField1" placeholder="Digite algo...">
+                            </div>
+                            <div class="form-group col-md-6">
+                                <label for="inputField2">Vagas remuneradas:</label>
+                                <input type="email" class="form-control" id="inputField2" placeholder="Digite algo...">
+                            </div>
+                        </div>
+                        <div class="form-row">
+                            <div class="form-group col-md-6">
+                                <label for="inputField3">Vagas voluntárias:</label>
+                                <input type="number" class="form-control" id="inputField3" placeholder="Digite algo...">
+                            </div>
+                            <div class="form-group col-md-6">
+                                <label for="inputField4">Data Ingresso:</label>
+                                <input type="date" class="form-control" id="inputField4" placeholder="Digite algo...">
+                            </div>
+                        </div>
+                        <div class="form-row">
+                            <div class="form-group col-md-6">
+                                <label for="inputField5">Telefone:</label>
+                                <input type="text" class="form-control" id="inputField5" placeholder="Digite algo...">
+                            </div>
+                            <div class="form-group col-md-6">
+                                <label for="inputField6">Matrícula:</label>
+                                <input type="text" class="form-control" id="inputField6" placeholder="Digite algo...">
+                            </div>
+                        </div>
+                    </div>
